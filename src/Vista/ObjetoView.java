68ff78518c994f1d3a3091b50999151071ab2bfb
Alexis <kennypallchizaca@gmail.com>
2024-05-29 20:04:19 -0500

    creado ObjetoView

---
+++ src/Vista/ObjetoView.java
@@ -0,0 +1,9 @@
+package Vista;
+
+public class ObjetoView {
+    public static void mostrarObjeto(Object obj) {
+        System.out.println("Detalles del objeto:");
+        System.out.println(obj.toString());
+    }
+
+}
