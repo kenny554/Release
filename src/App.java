88c2d24b635fdeeb1aab78e41e44e9be1cbaf904
Alexis <kennypallchizaca@gmail.com>
2024-05-29 19:53:17 -0500

    [Appropriate message]

--- src/App.java
+++ src/App.java
@@ -1,5 +1,16 @@
+import java.util.ArrayList;
+
+import Modelo.Celular;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Hola, World!");
+        ArrayList<Celular> celular1 = new ArrayList<>();
+        celular1.add(new Celular("Phone", 7));
+        celular1.add(new Celular("Phone", 8));
+        celular1.add(new Celular("Phone", 9));
+        celular1.add(new Celular("Phone", 10));
+        celular1.add(new Celular("Phone", 11));
+
     }
 }
--- src/Modelo/Celular.java
+++ src/Modelo/Celular.java
@@ -1,5 +1,28 @@
 package Modelo;
 
 public class Celular {
+    private String modelo;
+    private int numero;
+
+    public Celular(String modelo, int numero) {
+        this.modelo = modelo;
+        this.numero = numero;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
 
 }
